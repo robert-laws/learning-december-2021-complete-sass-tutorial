d13804102b7418897c3577c5e795b37a2bf97ab5
Robert Laws <roblaws@me.com>
2021-12-17 16:58:21 +0300

    reorganize sass file structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('src/sass/*.scss').pipe(sass()).pipe(dest('css'));
+  return src('sass/**/*.scss').pipe(sass()).pipe(dest('css'));
 }
 
 function watchTask() {
-  watch(['src/sass/**/*.scss'], buildStyles);
+  watch(['sass/**/*.scss'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
